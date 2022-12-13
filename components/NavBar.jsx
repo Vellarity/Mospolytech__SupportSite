38d62cdaec78783f2ac86fd8b0e4857a4869bbbc
Vellarity <Nikitaaorta@ya.ru>
2022-12-13 20:30:53 +0300

    NavBar update
    Signed-off-by: Vellarity <Nikitaaorta@ya.ru>

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,12 @@
 
 import Image from "next/image"
 import Link from "next/link"
-import LogoImg from "../public/assets/darkLogo.jpg"
 
 export const NavBar = () =>{
     return(
         <div className="w-full min-h-min h-20 bg-white border-b mb-5">
             <div className="container h-full mx-auto flex justify-between items-center">
-                <Image src={LogoImg} className="h-12 w-fit" alt="..." />
+                <Image src="/assets/darkLogo.jpg" className="h-12 w-fit" alt="..." />
                 
                 <div className="flex gap-x-3 text-lg font-semibold">
                     <Link className="hover:text-cyan-600 duration-200" href="/">Главная</Link>
